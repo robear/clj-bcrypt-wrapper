068dab9ea14631320e008c5ec1174efb8ea2c4e9
Robert Beaupre <robert@robear.ca>
2016-08-16 00:00:56 -0700

    Updated project version due to changes in dependencies.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clj-bcrypt-wrapper "0.1.0"
+(defproject clj-bcrypt-wrapper "0.2.0"
   :description "A very simple wrapper around the org.mindrot.jbcrypt library."
   :url "https://github.com/robear/clj-bcrypt-wrapper"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [de.svenkubiak/jBCrypt "0.4.1"]])
